9f7fc524a40a7a7ab012b5d9c95c4fb43ee1bfd8
Sergey Manshin <vvintpann@gmail.com>
2020-03-29 15:13:35 +0300

    add creating users feat

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      flash[:success]='Пользователь создан!'
+      #redirect_to root_path
+    else
+      @errors=@user.errors.full_messages
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  before_save{self.email.downcase!}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :name, length: {minimum: 2, maximum: 50}
+  validates :email, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, length: {minimum: 6}, allow_blank: true
+  has_secure_password
 end
