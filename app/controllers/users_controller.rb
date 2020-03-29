d4b36a127874e61cc7688a7ead32cab320db5de9
Sergey Manshin <vvintpann@gmail.com>
2020-03-29 16:17:29 +0300

    add header and pages controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @user=User.new(user_params)
     if @user.save
       flash[:success]='Пользователь создан!'
-      #redirect_to root_path
+      redirect_to root_path
     else
       @errors=@user.errors.full_messages
       render 'new'
