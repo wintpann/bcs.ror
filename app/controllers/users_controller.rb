ce8b78ab83c89dec62bfb97f5cdb6bd6ca1bfece
Sergey Manshin <vvintpann@gmail.com>
2020-03-29 16:32:14 +0300

    add edit user feat

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,26 @@ class UsersController < ApplicationController
   def create
     @user=User.new(user_params)
     if @user.save
-      flash[:success]='Пользователь создан!'
+      flash[:success]='User created'
       redirect_to root_path
     else
       @errors=@user.errors.full_messages
       render 'new'
+    end
+  end
+
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success]='User updated'
+      redirect_to root_path
+    else
+      @errors=@user.errors.full_messages
+      render 'edit'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'signup'=>'users#new'
   post 'signup'=>'users#create'
+  resources :users, only: [:edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
