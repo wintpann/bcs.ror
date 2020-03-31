6c4265b40be135625492d345bc1019bd10f44db5
Sergey Manshin <vvintpann@gmail.com>
2020-03-31 23:56:34 +0300

    rename senseless var to sensful

--- app/models/shopping_event.rb
+++ app/models/shopping_event.rb
@@ -3,9 +3,9 @@ class ShoppingEvent < ApplicationRecord
   belongs_to :all_event
 
   def self.create_event(options={})
-    event=self.create(product: options[:product], amount: options[:amount])
-    event.all_event.update_attribute(:sum, event.all_event.sum+options[:product].price_in*options[:amount])
+    shopping_event=self.create(product: options[:product], amount: options[:amount])
+    shopping_event.all_event.update_attribute(:sum, event.all_event.sum+options[:product].price_in*options[:amount])
 
-    event.all_event.user.warehouses.change_product(product: options[:product], amount: options[:amount])
+    shopping_event.all_event.user.warehouses.change_product(product: options[:product], amount: options[:amount])
   end
 end
