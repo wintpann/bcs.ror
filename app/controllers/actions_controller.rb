e4a8b0ebdfbebd1d2031aeeb63281e97a0d3bf72
Sergey Manshin <vvintpann@gmail.com>
2020-04-01 11:40:11 +0300

    refactoring

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -14,33 +14,8 @@ class ActionsController < ApplicationController
   end
 
   def events
-    all_events=@user.all_events.all
-    @events={}
-
-    old_date=to_local(all_events.first.created_at)[0..-7]
-    common_events=[]
-
-    all_events.each do |event|
-      new_date=to_local(event.created_at)[0..-7]
-      if event==all_events.last
-        if new_date==old_date
-          common_events << event
-          @events["#{old_date}"]=common_events
-        else
-          @events["#{old_date}"]=common_events
-          @events["#{new_date}"]=[event]
-        end
-      else
-        if new_date==old_date
-          common_events << event
-        else
-          @events["#{old_date}"]=common_events
-          common_events=[]
-          old_date=new_date
-          common_events << event
-        end
-      end
-    end
+    all_events=@user.all_events.all.order(created_at: :desc)
+    @events=to_hash_of_arrays_by_date(all_events)
   end
 
   def event
--- app/helpers/actions_helper.rb
+++ app/helpers/actions_helper.rb
@@ -1,5 +1,32 @@
 module ActionsHelper
-  def to_hash_of_arrays_by_date(events)
-    
+  def to_hash_of_arrays_by_date(all_events)
+    @events={}
+
+    old_date=to_local(all_events.first.created_at)[0..-7]
+    common_events=[]
+
+    all_events.each do |event|
+      new_date=to_local(event.created_at)[0..-7]
+      if event==all_events.last
+        if new_date==old_date
+          common_events << event
+          @events["#{old_date}"]=common_events
+        else
+          @events["#{old_date}"]=common_events
+          @events["#{new_date}"]=[event]
+        end
+      else
+        if new_date==old_date
+          common_events << event
+        else
+          @events["#{old_date}"]=common_events
+          common_events=[]
+          old_date=new_date
+          common_events << event
+        end
+      end
+    end
+    @events
   end
+
 end
