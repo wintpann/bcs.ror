947795b94ac6e89db4c81c1c3e9d67e7d663955c
Sergey Manshin <vvintpann@gmail.com>
2020-04-02 02:05:33 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,33 +37,34 @@ sunflower=taras.products.create!( name: 'sunflower',
                         price_in: 160,
                         price_out: 210 )
 
-taras.employees.create!(name: 'Misha',
+misha=taras.employees.create!(name: 'Misha',
                         fixed_rate: 500,
                         interest_rate: 10 )
 
-taras.employees.create!(name: 'Dima',
+dima=taras.employees.create!(name: 'Dima',
                         fixed_rate: 800,
                         interest_rate: 2 )
 
-taras.employees.create!(name: 'Lera',
+lera=taras.employees.create!(name: 'Lera',
                         fixed_rate: 600,
                         interest_rate: 8 )
 
-taras.employees.create!(name: 'Sasha',
+sasha=taras.employees.create!(name: 'Sasha',
                         fixed_rate: 500,
                         interest_rate: 15 )
 
-taras.employees.create!(name: 'Maxim',
+maxim=taras.employees.create!(name: 'Maxim',
                         fixed_rate: 400,
                         interest_rate: 18,
                         active: false )
 
-taras.employees.create!(name: 'Danil',
+danil=taras.employees.create!(name: 'Danil',
                         fixed_rate: 300,
                         interest_rate: 26,
                         active: false )
 
 first_event=taras.all_events.create(event_type: 'shopping')
+first_event.update_attribute(:created_at, '2020-03-28 22:38:39')
 
 first_event.shopping_events.create_event(product: orange,
                                         amount: 50 )
@@ -75,9 +76,22 @@ first_event.shopping_events.create_event(product: sunflower,
                                         amount: 70 )
 
 second_event=taras.all_events.create(event_type: 'throwing')
+second_event.update_attribute(:created_at, '2020-03-28 22:38:39')
 
 second_event.throwing_events.create_event(product: orange,
                                         amount: 20 )
 
 second_event.throwing_events.create_event(product: bread,
                                         amount: 10 )
+
+third_event=taras.all_events.create(event_type: 'start_work_session')
+third_event.update_attribute(:created_at, '2020-03-29 22:38:39')
+
+third_event.create_start_work_session_event(employee: misha)
+
+fourth_event=taras.all_events.create(event_type: 'giving')
+fourth_event.update_attribute(:created_at, '2020-03-29 22:38:39')
+
+fourth_event.giving_events.create_event(product: orange, amount: 4, employee: misha)
+
+fourth_event.giving_events.create_event(product: bread, amount: 9, employee: misha)
