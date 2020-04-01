b4c740a8b6407a1a9d3b7317e3d2bc59d39966a5
Sergey Manshin <vvintpann@gmail.com>
2020-04-02 01:36:20 +0300

    fix relations

--- app/models/all_event.rb
+++ app/models/all_event.rb
@@ -3,5 +3,5 @@ class AllEvent < ApplicationRecord
   has_many :shopping_events
   has_many :throwing_events
   has_many :giving_events
-  has_many :start_work_session_events
+  has_one :start_work_session_events
 end
